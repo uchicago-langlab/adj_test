b7d901271d286b5e308f03cb7b22a5bbb482804d
uchicago-langlab <86124521+uchicago-langlab@users.noreply.github.com>
2021-06-27 11:47:17 -0500

    Add files via upload

--- data_includes/adjprior.js
+++ data_includes/adjprior.js
@@ -51,8 +51,8 @@ var defaults = [
         startValue: 0,
         endValue: 100,
         hideProgressBar: true,
-        // scaleLabels: true,
-        // leftLabel: "(Totally unlikely)", rightLabel: "(Totally likely)"
+        scaleLabels: true,
+        leftLabel: "(Totally unlikely)", rightLabel: "(Totally likely)"
     },
     "Scale_NoButton3", {
         startValue: 0,
